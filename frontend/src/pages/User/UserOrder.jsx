d6b389e5732f98a430946dba1220e76404cd2e96
Pirijatharsan <86178608+piriyatharsan@users.noreply.github.com>
2024-12-21 00:16:26 +0530

    update UserOrder component to display total price in LKR and change button color

--- frontend/src/pages/User/UserOrder.jsx
+++ frontend/src/pages/User/UserOrder.jsx
@@ -39,7 +39,7 @@ const UserOrder = () => {
 
                 <td className="py-2">{order._id}</td>
                 <td className="py-2">{order.createdAt.substring(0, 10)}</td>
-                <td className="py-2">$ {order.totalPrice}</td>
+                <td className="py-2">LKR {order.totalPrice}</td>
 
                 <td className="py-2">
                   {order.isPaid ? (
@@ -67,7 +67,7 @@ const UserOrder = () => {
 
                 <td className="px-2 py-2">
                   <Link to={`/order/${order._id}`}>
-                    <button className="bg-pink-400 text-back py-2 px-3 rounded">
+                    <button className="bg-yellow-400 text-back py-2 px-3 rounded">
                       View Details
                     </button>
                   </Link>
