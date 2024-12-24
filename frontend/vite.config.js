d1f1eaad48ec695c4211a6c6ccd97a10df665598
Pirijatharsan <86178608+piriyatharsan@users.noreply.github.com>
2024-12-24 19:38:49 +0530

    update Vite configuration to change API and uploads proxy to production server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/': 'http://localhost:5000',
-      '/uploads/': 'http://localhost:5000',
+      '/api/': 'http://13.61.157.8:5000',
+      '/uploads/': 'http://13.61.157.8:5000',
     },
   },
 })
+
+//http://localhost:5000
+//http://localhost:5000
